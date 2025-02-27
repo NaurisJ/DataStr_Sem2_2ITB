2db7821c377194d9c1ce3a57477e265960b6292b
NaurisJ <nauris.javalds@gmail.com>
2025-02-27 10:21:05 +0200

    EDIT: added MyLinkedList with variables and isEmpty, isFull,length functions

---
+++ src/datastr/MyLinkedList.java
@@ -0,0 +1,36 @@
+package datastr;
+
+public class MyLinkedList {
+	private MyNode firstNode = null;
+	private MyNode lastNode = null;
+	private int counter = 0;
+
+	
+	// var netaisit bezargumenta konstruktoru, jo tas bus no java object klases
+	// get un set nevajag taisit frieks firstnode un lastnode, jo lietotajam nevajag laut piekluvi
+	// counter bus tikai get funkcija, jo ar set nedrikst laut lietotajam mainit
+	
+	public int getCounter() {
+		return counter;
+	}
+	
+	
+	public int length() {
+		return counter;
+	}
+	
+	public boolean isEmpty() {
+		return (counter == 0);
+	}
+	
+	// loti iespejams, ka nekad neizpildsisies, jo OS nodrosinas RAM parvaldibu
+	private boolean isFull() {
+		try {
+			new MyNode<Integer>(45); // meginam RAM atmina ielikt mezglu
+			return false; // ja tas izdodas tad saraksts nav pilns
+		} catch(OutOfMemoryError e) {
+			return true; // bet ja mezglu nevar vairs RAM atmina ielikt, tad saraksts ir pilns
+		}
+	}
+
+}
