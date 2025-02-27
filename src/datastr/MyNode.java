05049b6f60ebfe295377e00f65f7d1f1d2973ebe
NaurisJ <nauris.javalds@gmail.com>
2025-02-27 09:56:31 +0200

    EDIT: added MyNode class

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,46 @@
 package datastr;
 
-public class MyNode {
-
+public class MyNode <Ttype> {
+	private Ttype element;
+	private MyNode next = null;
+	private MyNode previous = null;
+	
+	
+	
+	public Ttype getElement() {
+		return element;
+	}
+	public MyNode getNext() {
+		return next;
+	}
+	public MyNode getPrevious() {
+		return previous;
+	}
+	
+	
+	public void setElement(Ttype element) {
+		if (element != null) {
+			this.element = element;
+		} else {
+			element = (Ttype) new Object();
+		}
+	}
+	
+	public void setNext(MyNode inputNext) {
+		next = inputNext;
+	}
+	
+	public void setPrevious(MyNode inputPrevious) {
+		previous = inputPrevious;
+	}
+	
+	
+	public MyNode(Ttype inputElement) {
+		setElement(inputElement);
+	}
+	
+	public String toString() {
+		return element + " ";
+	}
+	
 }
